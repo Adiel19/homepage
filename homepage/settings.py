7736b2f06368abba7ccc8c91fdfcf69eec0cadbf
Adiel19 <adielgranados.19@gmail.com>
2015-06-19 06:18:44 -0500

    update to have a local_settings file

--- homepage/settings.py
+++ homepage/settings.py
@@ -103,3 +103,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
