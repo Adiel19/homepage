4487f2135b40c2e8526401fd4cb1f92d930f6b64
Adiel19 <adielgranados.19@gmail.com>
2015-06-29 20:55:47 -0500

    remove say_hi and add welcome

--- homepage/urls.py
+++ homepage/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from say_hi.views import home
+from welcome.views import home
 
 urlpatterns = [
     url(r'^$', home),
--- say_hi/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class entry(models.Model):
-    name = models.CharField(max_length=45)
-    text = models.TextField()
